9166de04a49cb9343ab2b69a2e89cd33a37bcd06
echeng91 <cheng.eric612@gmail.com>
2016-04-14 16:16:04 -0400

    added job types

---
+++ SQL scripts/AddJobTypes.sql
@@ -0,0 +1,3 @@
+insert into ch_jobtype (job_id, job_code, job_description) values(1, 'ACCT', 'Accountant');
+insert into ch_jobtype (job_id, job_code, job_description) values(2, 'PROG', 'Programmer');
+insert into ch_jobtype (job_id, job_code, job_description) values(3, 'BSAT', 'Business Analyst');
