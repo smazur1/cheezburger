8cc008c4b0f2948505ad03644844d7d567936c3f
Chenye Yin <elizebethyin@gmail.com>
2016-04-14 16:19:27 -0400

    roles

---
+++ SQL scripts/chenye_export.sql
@@ -0,0 +1,20 @@
+
+Insert into ORA1.CH_HRROLE (HR_ID,ROLE_CODE,ROLE_DESCRIPTION) values (1,'HRM','HR Manager');
+Insert into ORA1.CH_HRROLE (HR_ID,ROLE_CODE,ROLE_DESCRIPTION) values (2,'HRS','HR Specialist');
+Insert into ORA1.CH_HRROLE (HR_ID,ROLE_CODE,ROLE_DESCRIPTION) values (3,'HRA','HR Assistant');
+Insert into ORA1.CH_HRROLE (HR_ID,ROLE_CODE,ROLE_DESCRIPTION) values (4,'HRH','Healthcare Professional');
+Insert into ORA1.CH_HRROLE (HR_ID,ROLE_CODE,ROLE_DESCRIPTION) values (5,'HRC','Compliance Officer');
+Insert into ORA1.CH_HRROLE (HR_ID,ROLE_CODE,ROLE_DESCRIPTION) values (6,'HIM','Hiring Manager');
+Insert into ORA1.CH_HRROLE (HR_ID,ROLE_CODE,ROLE_DESCRIPTION) values (7,'HGI','Group Interviewer');
+
+
+
+Insert into ORA1.CH_ACTIVITY (ACTID,ACTCODE,ACTDESCRIPTION) values (1,'NT','Nationality Check');
+Insert into ORA1.CH_ACTIVITY (ACTID,ACTCODE,ACTDESCRIPTION) values (2,'WR','Work History and Reference Check');
+Insert into ORA1.CH_ACTIVITY (ACTID,ACTCODE,ACTDESCRIPTION) values (3,'DV','Degree Validation');
+Insert into ORA1.CH_ACTIVITY (ACTID,ACTCODE,ACTDESCRIPTION) values (4,'DS','Drug Screen Test');
+Insert into ORA1.CH_ACTIVITY (ACTID,ACTCODE,ACTDESCRIPTION) values (5,'VS','Veteran Status Check');
+Insert into ORA1.CH_ACTIVITY (ACTID,ACTCODE,ACTDESCRIPTION) values (6,'HI','Human Resource Interview');
+Insert into ORA1.CH_ACTIVITY (ACTID,ACTCODE,ACTDESCRIPTION) values (7,'MI','Hiring Manager Interview');
+Insert into ORA1.CH_ACTIVITY (ACTID,ACTCODE,ACTDESCRIPTION) values (8,'GI','Group Interview');
+Insert into ORA1.CH_ACTIVITY (ACTID,ACTCODE,ACTDESCRIPTION) values (9,'CT','Coding Test');
