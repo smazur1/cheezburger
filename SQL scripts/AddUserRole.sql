9e932faefbc32a1b8c25388b27034dff6f8d044a
echeng91 <cheng.eric612@gmail.com>
2016-04-14 16:32:55 -0400

    sql add user roles

---
+++ SQL scripts/AddUserRole.sql
@@ -0,0 +1,7 @@
+insert into ch_user_role(userroleid, userid, rolecode) values (1, 1, 'HRM');
+insert into ch_user_role(userroleid, userid, rolecode) values (2, 2, 'HRS');
+insert into ch_user_role(userroleid, userid, rolecode) values (3, 3, 'HRA');
+insert into ch_user_role(userroleid, userid, rolecode) values (4, 4, 'HRH');
+insert into ch_user_role(userroleid, userid, rolecode) values (5, 5, 'HRC');
+insert into ch_user_role(userroleid, userid, rolecode) values (6, 6, 'HIM');
+insert into ch_user_role(userroleid, userid, rolecode) values (7, 7, 'HGI');
