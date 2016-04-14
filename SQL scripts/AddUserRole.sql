462e7d0a9f6a3250590c1ee826322560b1b4653e
echeng91 <cheng.eric612@gmail.com>
2016-04-14 16:49:15 -0400

    updated inserts to match new table format

--- SQL scripts/AddUserRole.sql
+++ SQL scripts/AddUserRole.sql
@@ -1,7 +1,7 @@
-insert into ch_user_role(userroleid, userid, rolecode) values (1, 1, 'HRM');
-insert into ch_user_role(userroleid, userid, rolecode) values (2, 2, 'HRS');
-insert into ch_user_role(userroleid, userid, rolecode) values (3, 3, 'HRA');
-insert into ch_user_role(userroleid, userid, rolecode) values (4, 4, 'HRH');
-insert into ch_user_role(userroleid, userid, rolecode) values (5, 5, 'HRC');
-insert into ch_user_role(userroleid, userid, rolecode) values (6, 6, 'HIM');
-insert into ch_user_role(userroleid, userid, rolecode) values (7, 7, 'HGI');
+insert into ch_user_role(userroleid, userid, rolecode) values (1, 1, 1);
+insert into ch_user_role(userroleid, userid, rolecode) values (2, 2, 2);
+insert into ch_user_role(userroleid, userid, rolecode) values (3, 3, 3);
+insert into ch_user_role(userroleid, userid, rolecode) values (4, 4, 4);
+insert into ch_user_role(userroleid, userid, rolecode) values (5, 5, 5);
+insert into ch_user_role(userroleid, userid, rolecode) values (6, 6, 6);
+insert into ch_user_role(userroleid, userid, rolecode) values (7, 7, 7);
