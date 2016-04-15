9d5b50a2f79f9840608ab0699f561fc304b34d37
Chenye Yin <elizebethyin@gmail.com>
2016-04-15 13:29:02 -0400

    what

---
+++ UnitTest/TestInsert.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.* ;
+
+import org.junit.Test;
+
+import customTools.DBUtil;;
+public class TestInsert {
+	@Test
+	   public void test_importsingleproduct() {
+		ProcessUser pp=new ProcessUser();
+	      System.out.println("Test if table has been sucessfully imported") ;
+	      int id=pp.UserLogin("user1", "pass1");
+	     // Subscription S = new Subscription(200,2) ;
+	      assertEquals(id,1) ;
+	   }
+}
