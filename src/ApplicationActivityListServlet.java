29f81dbb35bd1eaee786fc57abe362a215987c9e
echeng91 <cheng.eric612@gmail.com>
2016-04-18 10:13:01 -0400

    added more get by ID to DBUtil

---
+++ src/ApplicationActivityListServlet.java
@@ -0,0 +1,53 @@
+
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import customTools.DBUtil;
+import model.ChApplicationActivity;
+import model.ChUser;
+
+/**
+ * Servlet implementation class ApplicationActivityListServlet
+ */
+@WebServlet("/ApplicationActivityListServlet")
+public class ApplicationActivityListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApplicationActivityListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		ChUser current = (ChUser)session.getAttribute("user");
+		long appID = Long.parseLong(request.getParameter("appid"));
+		HashMap<ChApplicationActivity, String> accessMap = DBUtil.getAppActAccessMap(current.getChUserRoles().get(0).getChHrrole(), DBUtil.getApplicationByID(appID));
+		request.setAttribute("activitymap", accessMap);
+		request.getRequestDispatcher("").forward(request, response);
+	}
+
+}
