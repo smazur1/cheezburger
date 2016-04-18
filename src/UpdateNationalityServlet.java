b15da2496aa1985f434685f562f105cf7f4020d0
echeng91 <cheng.eric612@gmail.com>
2016-04-18 14:58:58 -0400

    UpdateNationalityServlet

---
+++ src/UpdateNationalityServlet.java
@@ -0,0 +1,65 @@
+
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import customTools.DBUtil;
+import model.ChApplication;
+import model.ChApplicationActivity;
+
+/**
+ * Servlet implementation class UpdateNationalityServlet
+ */
+@WebServlet("/UpdateNationalityServlet")
+public class UpdateNationalityServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateNationalityServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		Date now = new Date();
+		ChApplication application = (ChApplication) session.getAttribute("application");
+		String appActStatus = request.getParameter("status");
+		ChApplicationActivity appAct = DBUtil.getApplicationActivityByID(application.getAppid(), 1);
+		
+		appAct.setActmoddate(now);
+		appAct.setActstatus(appActStatus);
+		DBUtil.update(appAct);
+		
+		application.setModdate(now);
+		if(appActStatus.equalsIgnoreCase("F")) {
+			application.setAppstatus("F");
+		}
+		DBUtil.update(application);
+		
+		session.setAttribute("application", application);
+		request.getRequestDispatcher("ApplicationActivityListServlet");
+	}
+
+}
