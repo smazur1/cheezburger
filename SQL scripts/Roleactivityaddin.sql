6f7f04a26c2d7b12a423fb98be88c725035b9138
Chenye Yin <elizebethyin@gmail.com>
2016-04-19 10:41:25 -0400

    ROleactivityaddin

---
+++ SQL scripts/Roleactivityaddin.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Tuesday-April-19-2016   
+--------------------------------------------------------
+
+Insert into CH_ROLE_ACTIVITY (ROLEACTID,HR_ID,ACTID,RAACCESS) values (30,1,7,'0');
+Insert into CH_ROLE_ACTIVITY (ROLEACTID,HR_ID,ACTID,RAACCESS) values (31,1,8,'0');
+Insert into CH_ROLE_ACTIVITY (ROLEACTID,HR_ID,ACTID,RAACCESS) values (32,1,9,'0');
+Insert into CH_ROLE_ACTIVITY (ROLEACTID,HR_ID,ACTID,RAACCESS) values (33,6,8,'0');
