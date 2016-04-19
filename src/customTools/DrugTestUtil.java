d16e904dbfd5cf5702a5fddf40bb1cdfe4dd6e03
echeng91 <cheng.eric612@gmail.com>
2016-04-19 14:46:38 -0400

    removed unnecessary imports

--- src/customTools/DrugTestUtil.java
+++ src/customTools/DrugTestUtil.java
@@ -2,9 +2,6 @@ package customTools;
 
 import java.util.Date;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
