df9b169234869056d3b773ee2fb5dabc85fb43eb
echeng91 <cheng.eric612@gmail.com>
2016-04-19 13:58:21 -0400

    jsp and update servlet for activities 8-10

---
+++ WebContent/GroupInterview.jsp
@@ -0,0 +1,67 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Group Interview</title>
+</head>
+<body>
+<h2>Group Interview</h2>
+<!-- Show user info and list incomplete dependent activity -->
+<p> User: <c:out value="  ${user.username}" />         HR Role: <c:out value="  ${user.chUserRoles[0].chHrrole.roleDescription}" /></p>
+<p>
+	<c:if test="${depflag == \"1\"}">
+		<table>
+		  <tr>
+		    <th> Incomplete Dependent Activities</th>
+		  </tr>
+		  <c:forEach items="${depactlist}" var="depact">
+			<tr>
+				<td><c:out value="  ${depact.chActivity.actdescription}" /></td>
+			</tr>
+		  </c:forEach>
+		</table>
+	</c:if>
+	<br/>
+	<br/>
+
+	<table>
+		<tr>
+			<th> Application ID</th>
+			<th> Applicant Name</th>
+			<th> Application Status</th>
+		</tr>
+		<tr>
+			<td><c:out value="  ${application.appid}" /></td>
+			<td><c:out value="  ${application.name}" /></td>
+			<td><c:out value="  ${application.appstatus}" /></td>
+		</tr>
+	</table>
+
+		
+	<c:if test="${application.appstatus != \"F\" && depflag == \"0\"}">
+		<form action="UpdateGroupInterviewServlet" method="post">
+			<input type="radio" name="status" value="P">Confirm<br>
+			<input type="radio" name="status" value="F">Reject<br>
+			<input type="radio" name="status" value="I" checked>In process<br>
+			<br>
+			Comments: <br><textarea name="groupcomment" rows="8" cols="40" maxlength="255"><c:out value="${groupcomment}" /></textarea><br>
+			<input type="submit" value="Submit">
+		</form>	
+	</c:if>
+	
+	<form action="ApplicationActivityListServlet" method="post">
+		<input type="submit" value="Return to activity list">
+	</form>	
+
+	<form action="ApplicationListServlet" method="post">
+		<input type="submit" value="Return to application list">
+	</form>
+
+	<form action="LogoutServlet" method="post">
+		<input type="submit" value="Logout">
+	</form>	
+</body>
+</html>
---
+++ src/UpdateCodingTestServlet.java
@@ -0,0 +1,76 @@
+
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import customTools.DBUtil;
+import model.ChApplication;
+import model.ChApplicationActivity;
+import model.ChComment;
+
+/**
+ * Servlet implementation class UpdateCodingTestServlet
+ */
+@WebServlet("/UpdateCodingTestServlet")
+public class UpdateCodingTestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateCodingTestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String sendTo = "ApplicationActivityListServlet";
+		Date now = new Date();
+		ChApplication application = (ChApplication) session.getAttribute("application");
+		
+		String appActStatus = request.getParameter("status");
+		ChApplicationActivity appAct = DBUtil.getApplicationActivityByID(application.getAppid(), 9);
+		
+		appAct.setActmoddate(now);
+		appAct.setActstatus(appActStatus);
+		DBUtil.update(appAct);
+		
+		String alteredComment = request.getParameter("codingtestcomment");
+		ChComment currentComment = DBUtil.getCommentByAppActId(appAct.getAppactid());
+		
+		currentComment.setComments(alteredComment);
+		currentComment.setModdate(now);
+		DBUtil.update(currentComment);
+		
+		application.setModdate(now);
+		if(appActStatus.equalsIgnoreCase("F")) {
+			application.setAppstatus("F");
+			sendTo = "Rejection.jsp";
+		}
+		DBUtil.update(application);
+		
+		session.setAttribute("application", application);
+		request.getRequestDispatcher(sendTo).forward(request, response);
+	}
+
+}
